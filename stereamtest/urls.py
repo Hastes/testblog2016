272648f7f472b4192eca17a3fd9eab35f8ef0aa3
Hastes2 <rom541997@gmail.com>
2016-08-22 23:24:07 +0700

    'Nomodulenamed'

--- stereamtest/urls.py
+++ stereamtest/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.conf.urls import url,include
 from django.contrib import admin
-from testblog2016.blog import views
+from blog import views
 
 
 urlpatterns = [
