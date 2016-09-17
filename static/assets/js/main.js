f54c72a11ca8334a58941d0858f2940e2b683010
hs <rom541997@gmail.com>
2016-09-17 22:51:12 +0700

    frbug

--- static/assets/js/main.js
+++ static/assets/js/main.js
@@ -63,10 +63,8 @@
 		// Search (header).
 			var $search = $('#search'),
 				$search_input = $search.find('input');
-
 			$body
 				.on('click', '[href="#search"]', function(event) {
-
 					event.preventDefault();
 
 					// Not visible?
