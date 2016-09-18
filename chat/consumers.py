fc8c312deb989b126dc3d8c87662d17970a68a28
hs <rom541997@gmail.com>
2016-09-18 16:56:53 +0700

    chat

--- chat/consumers.py
+++ chat/consumers.py
@@ -8,8 +8,13 @@ def ws_connect(message):
     Group('chat').add(message.reply_channel)
 
 def ws_message(message):
-    Group('chat').send({'text': json.dumps({'message': message.content['text'],
-                                            'sender': message.reply_channel.name})})
+    try:
+       data = json.loads(message['text'])
+    except ValueError:
+       print("ws message isn't json text=%s", message['text'])
+       return
+    Group('chat').send({'text': json.dumps({'message': data,
+                                            'sender': message.reply_channel.name})}),
 
 def ws_disconnect(message):
     Group('chat').discard(message.reply_channel)
