a0fc806fa8b24cc0b1ec04c43483bdf5b6fd749f
hs <rom541997@gmail.com>
2016-10-02 16:22:45 +0700

    staticfix

--- stereamtest/urls.py
+++ stereamtest/urls.py
@@ -61,6 +61,8 @@ urlpatterns = [
 import os
 from django.conf.urls.static import static
 
+
+
 if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
