4eed4bac0d43bce5a28fad95194c457c62a6accd
hs <rom541997@gmail.com>
2016-10-02 16:35:06 +0700

    fixstc

--- stereamtest/asgi.py
+++ stereamtest/asgi.py
@@ -4,3 +4,7 @@ import channels.asgi
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'stereamtest.settings')
 
 channel_layer = channels.asgi.get_channel_layer()
+
+from whitenoise.django import DjangoWhiteNoise
+
+application = DjangoWhiteNoise(channel_layer)
